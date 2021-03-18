ea504d54dcdd693fc0a1e9269e26d166b86efcfd
unknown <you@example.com>
2021-03-18 14:45:09 -0500

    created style components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React , {useState, useEffect}from "react";
 import "./App.css";
 import {BASE_URL,URL_KEY} from './constant/constant'//Importing Props of the the api url and key
 import axios from 'axios'
+import styled from 'styled-components'
 
 function App() {
 
@@ -19,10 +20,69 @@ function App() {
     console.log('Houston we have problem')//if there is an error "Houston we have a problem will display in the DOM"
   })
   }, [])
-  
 
+  const StyledFriend = styled.div`
+  
+    background: purple;
+    text-align: center;
+
+    &:hover{
+      background: blue;
+      transition: 5s
+      
+    }
+    transition: all 5s ease-in;
+
+    img{
+      max-width: 100%;
+      max-height: 100%;
+      display: block;
+      border-radius: 30px;
+
+      &:hover {
+      transform: scale(1.2);
+      transition: all 5s ease-in-out;
+    }
+    transition: all 5s ease-in;
+      }
+    .imgContainer{
+      width: 600px;        
+      border: 5px solid black;
+      border-radius: 36px;
+      }
+    .copyright{
+      margin-left: 10px;
+      width: 350px;
+      font-size: large;
+      font-weight: bold;
+      }
+    .explanation{
+      /* border: 5px solid black; */
+      margin-top: 20px;
+      width: 600px;
+      }
+    .container{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      }
+    .dcContainer{
+      display: flex;
+      flex-direction: row;
+      margin-top: 20px;
+      }
+    .date{
+      margin-right: 10px;
+      width: 250px;
+      text-decoration: solid;
+      font-size: large;
+      font-weight: bold;
+      }
+    `
+  
   return (
-    <div className="App">
+    <StyledFriend>
       <div className = 'container'>
        <h1>{data.title}</h1>
        <div className = 'imgContainer'>
@@ -35,7 +95,7 @@ function App() {
 
        <div className = 'explanation'>{data.explanation}</div>
       </div>
-    </div>
+    </StyledFriend>
   );
 }
 
